9cc936867ec5a7cdbbba3107be41caf46310045b
abnerjunior <abnerxvx@gmail.com>
2019-12-09 14:45:30 -0300

    descripcion

--- database/migrations/2019_12_06_223254_create_semestres_materias_table.php
+++ database/migrations/2019_12_06_223254_create_semestres_materias_table.php
@@ -13,32 +13,32 @@ class CreateSemestresMateriasTable extends Migration
      */
     public function up()
     {
-        Schema::create('semestres_materias', function (Blueprint $table) {
+        Schema::create('semestres_materias', function (Blueprint $table) 
+        {
             $table->bigIncrements('id_sm');
+           
+            $table->timestamps();
+        });
+
+        Schema::table('semestres_materias', function($table)
+        {
+            
             $table->unsignedBigInteger('cod_materia');
             $table->unsignedBigInteger('cod_semestres');
             $table->unsignedBigInteger('cod_usuario');
             $table->unsignedBigInteger('cod_aula');
             
             $table->foreign('cod_materia')
-            ->references('cod_materia')
-            ->on('materias')
-            ->onDelete('cascade');
+            ->references('cod_materia')->on('materias');
 
             $table->foreign('cod_usuario')
-            ->references('cod_usuario')
-            ->on('usuarios')
-            ->onDelete('cascade');
-
-            $table->foreign('cod_aula')
-            ->references('cod_aula')
-            ->on('aulas')
-            ->onDelete('cascade');
+            ->references('cod_usuario')->on('usuarios');
 
             $table->foreign('cod_semestres')
-            ->references('cod_semestres')
-            ->on('semestres')
-            ->onDelete('cascade');
+            ->references('cod_semestres')->on('semestres');
+
+            $table->foreign('cod_aula')
+            ->references('cod_aula')->on('aulas'); 
             
         });
     }
