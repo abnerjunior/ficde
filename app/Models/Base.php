02021141e2692019ded941c731eda80599b0cfc3
abnerjunior <abnerxvx@gmail.com>
2019-12-17 17:56:07 -0300

    delete user

--- app/Models/Base.php
+++ app/Models/Base.php
@@ -16,25 +16,26 @@ class Base extends Model
 	 * @return results data
 	 */
 
-    public static function search (array $data = array(), $q) {
+	public static function search(array $data = array(), $q)
+	{
 
 		if (!empty($data['dataSearch'])) {
-        	$fields = json_decode($data['dataSearch'], true);
-        	$fields = array_filter($fields, 'strlen');
-        	$fields = array_only($fields, static::$filterable);
-	        foreach ($fields as $field => $value) {
-	            if (isset($fields[$field])) {
-					$q->where('status', 'y')
-	                ->orWhere($field, 'LIKE', "%$fields[$field]%")->orderBy($data['sortField'], $data['sortOrder']);
-	            }
-	        }
-    	}
-
-        if ($data['paginate'] === "true") {
-        	return $q->paginate($data['perPage']);
-        } else {
-        	return $q->get();
-        }
+			$fields = json_decode($data['dataSearch'], true);
+			$fields = array_filter($fields, 'strlen');
+			$fields = array_only($fields, static::$filterable);
+			$q->where(function ($query) use ($fields, $data) {
+				foreach ($fields as $field => $value) {
+					if (isset($fields[$field])) {
+						$query->orWhere($field, 'LIKE', "%$fields[$field]%")->orderBy($data['sortField'], $data['sortOrder']);
+					}
+				}
+			});
+		}
+		$q->where('status', 'y');
+		if ($data['paginate'] === "true") {
+			return $q->paginate($data['perPage']);
+		} else {
+			return $q->get();
+		}
 	}
-
 }
