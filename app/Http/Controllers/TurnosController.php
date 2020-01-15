7e51eea625d76ca3b726bf5807821e7114e3985b
palma001 <palmaluis1997@gmail.com>
2020-01-15 14:18:55 -0400

    Update TurnosController.php

--- app/Http/Controllers/TurnosController.php
+++ app/Http/Controllers/TurnosController.php
@@ -21,7 +21,7 @@ class TurnosController extends Controller
         if ($turno !== null) {
             $unique = Rule::unique('turnos')->ignore($request->turno, 'turno');
         } else {
-            $unique = 'unique:cod_turno';
+            $unique = 'unique:turno';
         }
         $validator = Validator::make($request->all(), [
             'turno' => ['required', 'max:10', $unique],
