6404a81603f1dadad5affba30de342038d11eb53
palma001 <palmaluis1997@gmail.com>
2020-01-16 16:48:53 -0400

    Update AsistenciasController.php

--- app/Http/Controllers/AsistenciasController.php
+++ app/Http/Controllers/AsistenciasController.php
@@ -140,9 +140,14 @@ class AsistenciasController extends Controller
             'estudiantes.nombre as nombreEstudiante',
             'estudiantes.apellido as apellidoEstudiante',
             'estudiantes.dni as dni',
-            'asistencias.*'
+            'asistencias.*',
+            'materias.materia'
         )
         ->join('estudiantes', 'estudiantes.cod_estudiante', 'asistencias.id_estudiante');
+        ->join('estudiantes_materias', 'estudiantes_materias.cod_em', 'notas.id_em')
+        ->join('semestres_materias', 'semestres_materias.cod_sm', 'estudiantes_materias.id_semestre')
+        ->join('materias', 'materias.cod_materia', 'semestres_materias.id_materia');
+        $notas = notas::search($request->toArray(), $q,'notas');
         $asistencias = asistencias::search($request->toArray(), $q,'asistencias');
         return  new UsersCollection($asistencias);
     }
