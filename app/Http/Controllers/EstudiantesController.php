f77d5ac5b0ccb0ab055dcd914f3b59622aa1aae2
Luis Palma <palmaluis1997@gmail.com>
2020-03-17 08:57:14 -0430

    id estudiante

--- app/Http/Controllers/EstudiantesController.php
+++ app/Http/Controllers/EstudiantesController.php
@@ -318,7 +318,7 @@ class EstudiantesController extends Controller
                 $errors = $this->validation($request, $id)->errors();
                 return response()->json($errors->all(), 400);
             } else {
-                DB::table('curso_estudiantes')->where('id', $id)->delete();
+                DB::table('curso_estudiantes')->where('id_estudiante', $id)->delete();
                 $this->storeCourses($id, $request->cursos, $request->user_r);
                 return response()->json($request, 200);
             }
